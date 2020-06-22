9be5f27b9d656bb39e4cf762e52d3798d9089142
Odilomar Rebelo Rocha Junior <odilomar.junior@gmail.com>
2020-06-22 16:07:07 -0400

    :tada: Initial Commit with Challenge Template"

--- README.md
+++
@@ -1,2 +0,0 @@
-# GoStack-Repository-Mobile
-This application is a challenge in the GoStack Bootcamp at Rocketseat using React Native
---
+++ src/App.js
@@ -0,0 +1,107 @@
+import React from "react";
+
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  Text,
+  StatusBar,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+
+export default function App() {
+  async function handleLikeRepository(id) {}
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+
+      <SafeAreaView style={styles.container}>
+        <View style={styles.repositoryContainer}>
+          <Text style={styles.repository}>Repositório</Text>
+
+          <View style={styles.techsContainer}>
+            <Text style={styles.tech} key={`${tech}-${index}`}>
+              Node
+            </Text>
+            <Text style={styles.tech} key={`${tech}-${index}`}>
+              JS
+            </Text>
+            <Text style={styles.tech} key={`${tech}-${index}`}>
+              Express
+            </Text>
+          </View>
+
+          <View style={styles.likesContainer}>
+            <Text
+              style={styles.likeText}
+              testID={`repository-likes-1`}
+            >
+              1 curtidas
+            </Text>
+          </View>
+
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleLikeRepository(1)}
+            testID={`like-button-1`}
+          >
+            <Text style={styles.buttonText}>Curtir</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#7159c1",
+  },
+  repositoryContainer: {
+    marginBottom: 15,
+    marginHorizontal: 15,
+    backgroundColor: "#fff",
+    padding: 20,
+  },
+  repository: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  techsContainer: {
+    flexDirection: "row",
+    marginTop: 10,
+  },
+  tech: {
+    fontSize: 12,
+    fontWeight: "bold",
+    marginRight: 10,
+    backgroundColor: "#04d361",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    color: "#fff",
+  },
+  likesContainer: {
+    marginTop: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  likeText: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginRight: 10,
+  },
+  button: {
+    marginTop: 10,
+  },
+  buttonText: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginRight: 10,
+    color: "#fff",
+    backgroundColor: "#7159c1",
+    padding: 15,
+  },
+});
